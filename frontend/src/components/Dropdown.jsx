2af5efe631750449b659d4c8619de472e017d03e
Musa Quddus <musa.q@berkeley.edu>
2024-10-14 15:21:13 -0700

    fixed styling issue and arrows

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -1,13 +1,15 @@
+import { useState } from 'react'
 import '../css/style.css'
 import '../css/bootstrap.min.css'
 
 
 const Dropdown = ({name, data, onClick}) => {
+    const [expanded, setExpanded] = useState(false)
 
     return (
-        <div>
+        <div onMouseEnter={() => setExpanded(!expanded)} onMouseLeave={()=>setExpanded(false)}>
             <a className="nav-link dropdown-toggle" onClick = {onClick} id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {name} <span className="arrow"></span>
+                {name} <span className={expanded ? "up-arrow arrow" : "arrow"}></span>
             </a>
             <div className="dropdown-menu" aria-labelledby="navbarDropdown" id="compliancy-dropdown">
                 <ul>

e4c32e94f8b81c40273006f7892be289eb746511
Yusuf Quddus <yusufquddus@berkeley.edu>
2024-10-15 21:48:06 -0700

    remove unneeded files

--- js/compliancy.js
+++
@@ -1,7 +0,0 @@
-const compliancyData = [
-    { "name": "ISO 9000", "description": "Description for ISO 9000"}
-];
-
-compliancyData.forEach(item => {
-  item.href = `./compliancy/${item.name.replace(/\s+/g, '-').replace(/[^a-zA-Z0-9\-]/g, '')}.html`;
-});

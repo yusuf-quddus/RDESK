b2815dce1cc39765e0d19427b4f4c71b5978c10c
Yusuf Quddus <yusufquddus@berkeley.edu>
2024-10-16 18:00:45 -0700

    compliance page template

--- frontend/src/components/Compliance-Page.jsx
+++ frontend/src/components/Compliance-Page.jsx
@@ -1,29 +1,45 @@
 import React from 'react';
 import Header from './Header'; 
+import { useNavigate } from 'react-router-dom';
 import '../css/style.css'; 
 
 const CompliancePage = ({ iso, scrollToSection }) => {
+  const navigate = useNavigate()
+
   return (
     <div>
       <Header scrollToSection={scrollToSection} />
 
-      <div
-        className="compliance-page"
-        style={{
-          backgroundImage: `url(${iso.image})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      >
-        <div className="compliance-overlay">
-          <div className="compliance-container">
-            <h1 className="compliance-title">{iso.name}</h1>
-            <p className="compliance-description">{iso.description}</p>
+      <div className="tab-content-main">
+        <div className="container">
+                    <div className="big compliance-title text-center">
+                        <h2 className="title">{iso.name}</h2>
+                    </div>
+                    <div className="tab-content-2">   
+                        <div className="col-md-4">
+                            <div className="devices-image">
+                                <img src="../images/device-desktop.png" alt=""/>
+                            </div>
+                        </div>       
+                          <div className="col-md-8">
+                            <div className="core-features">      
+                                <p>{iso.description}</p>   
+                                <p>{iso.description}</p>
+                                <p>{iso.description}</p>
+                            </div>
+                            <div>
+                            <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px' }}> 
+                                <a onClick={() => navigate("/", { state: { targetId: "contact" } })} className="middle btn-gray-border hover-btn-blue">
+                                    Request Service
+                                </a>
+                            </div>
+                        </div>
+                        </div>  
+                    </div>
           </div>
-        </div>
       </div>
-    </div>
-  );
+      </div>
+    )
 };
 
 export default CompliancePage;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate, useLocation, Link } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import data from '../data.json';
 import Dropdown from './Dropdown';
 import '../css/style.css';

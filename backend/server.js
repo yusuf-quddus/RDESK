46cf7d549c1ddf31bee8b3da61e1a5170c212eee
Yusuf Quddus <yusufquddus@berkeley.edu>
2024-12-06 15:19:33 -0800

    error checking

--- backend/server.js
+++ backend/server.js
@@ -122,5 +122,6 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
+    console.error(PORT);
     console.log(`Server running on http://localhost:${PORT}`)
 })

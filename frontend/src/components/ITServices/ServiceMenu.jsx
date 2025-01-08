5f2e612c6c11471cf4ec39bba35a24bb6329f832
Yusuf Quddus <yusufquddus@berkeley.edu>
2025-01-08 14:15:04 -0800

    fix solutions landing button error and deploy

--- frontend/src/components/ITServices/ServiceMenu.jsx
+++ frontend/src/components/ITServices/ServiceMenu.jsx
@@ -2,6 +2,7 @@ import '../../css/style.css'
 import '../../css/bootstrap.min.css'
 
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 /**
  * Component for expandable core services section.  
@@ -11,6 +12,16 @@ import { useState } from 'react';
 const ServiceMenu = ({info}) => {
     const [expanded, setExpanded] = useState(false);
     const toggleCard = () => setExpanded(!expanded);
+    const navigate = useNavigate();
+
+    const handleItemClick = (pageLink) => {
+        if (pageLink) {
+            navigate(pageLink);
+        } else {
+            console.log('No page-link found for this item.');
+        }
+    };
+
     return (
         <div className="panel-default">
             <div className="panel-heading" onClick={toggleCard} 
@@ -24,10 +35,10 @@ const ServiceMenu = ({info}) => {
                 <div className="panel-body">
                     {info.description}
                     <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-                        <a onClick={() => handleItemClick('#')} className="middle btn-gray-border hover-btn-blue" style={{ marginRight: '10px' }}>
+                        <a onClick={() => handleItemClick(info['page-link'])} className="middle btn-gray-border hover-btn-blue" style={{ marginRight: '10px' }}>
                             Learn More
                         </a>
-                        <a onClick={() => navigate(`/contact#request-service#compliance-services#${'#'}`)} className="middle btn-gray-border hover-btn-blue">
+                        <a onClick={() => navigate(`/contact#request-service#it-services#${info.name}`)} className="middle btn-gray-border hover-btn-blue">
                             Request Service
                         </a>
                     </div>
